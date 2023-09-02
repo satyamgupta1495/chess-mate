abc7b735afac33ce8cc7859097719ebecdafefda
Satyam Gupta <satyamgupta1495@gmail.com>
2023-09-03 00:59:31 +0530

    made responsive

--- client/src/components/Controls/Controls.tsx
+++ client/src/components/Controls/Controls.tsx
@@ -1,12 +1,10 @@
 import { useState } from 'react';
 import useControl from './hooks/useControl';
-import logo from "../../assets/icon2.png"
-import { Button } from 'react-bootstrap';
+import { Button, Offcanvas } from 'react-bootstrap';
 import { GiArtificialIntelligence } from 'react-icons/gi';
 import { IoColorPalette } from 'react-icons/io5';
 import { FaUserFriends, FaRobot } from 'react-icons/fa';
-import { FaAngleDoubleRight, FaAngleDoubleLeft } from 'react-icons/fa'
-import { Sidebar, Menu, MenuItem } from 'react-pro-sidebar';
+import { RxHamburgerMenu } from 'react-icons/rx';
 
 
 type ControlsProps = {
@@ -21,7 +19,8 @@ function Controls({ setGame, setMode, setCurrTheme }: ControlsProps) {
 
     const { onThemeSelect } = useControl({ setGame, setMode, setCurrTheme })
 
-    const [collapsed, setCollapsed] = useState(false);
+    // const [collapsed, setCollapsed] = useState(false);
+    const [show, setshow] = useState(false);
 
     const [menu, setMenu] = useState({
         modeMenu: false,
@@ -42,46 +41,55 @@ function Controls({ setGame, setMode, setCurrTheme }: ControlsProps) {
         })
     }
 
-    function handleCollapse() {
-        setCollapsed(!collapsed)
+    function handleClose() {
+        setshow(!show)
     }
 
     return (
         <>
+            <div className='btn-container'>
+                <Button onClick={handleClose}>
+                    <RxHamburgerMenu />
+                </Button>
+            </div>
             <div className="side-bar-container">
-                <Sidebar rootStyles={{ border: "none" }}
-                    collapsed={collapsed} transitionDuration={500}>
-                    <div className='btn-container'>
-                        <Button onClick={handleCollapse}>{collapsed ? <FaAngleDoubleRight /> : <FaAngleDoubleLeft />}</Button>
-                        <div className="logo-container">
-                            <h3><span><img src={logo}></img> </span>{!collapsed && "Chessmate"}</h3>
+                <Offcanvas className="offcanvas" show={show} onHide={handleClose} styles={{ canvas: { width: '10px' } }}>
+                    <Offcanvas.Header closeButton>
+                        <Offcanvas.Title className='offcanvas-header' >Chessmate</Offcanvas.Title>
+                    </Offcanvas.Header>
+                    <Offcanvas.Body className='offcanvas-body'>
+
+                        <div className="sidebar-options">
+                            <div className='main-menu'>
+                                <p onMouseEnter={() => handleMouseEnter("Mode")}> <span className='menu-icons'><GiArtificialIntelligence /></span>Mode</p>
+                                <p onMouseEnter={() => handleMouseEnter("Theme")}> <span className='menu-icons'><IoColorPalette /></span>Theme</p>
+                            </div>
+                            <div className="controls">
+                                {menu.modeMenu &&
+                                    <div className="menu" onMouseLeave={handleMouseLeave}>
+                                        <Button value="Two Players" onClick={() => setMode("analyze")}>
+                                            <span className='menu-icons'><FaUserFriends /></span> Two Players</Button>
+                                        <Button value="Computer" onClick={() => setMode("random")}><span className='menu-icons'><FaRobot /></span> Computer</Button>
+                                    </div>
+                                }
+
+                                {menu.themeMenu &&
+                                    <div className="menu" onMouseLeave={handleMouseLeave}>
+                                        <Button value="Red" onClick={() => onThemeSelect("#769656")}>Default</Button>
+                                        <Button value="Red" onClick={() => onThemeSelect("red")}>Red</Button>
+                                        <Button value="Purple" onClick={() => onThemeSelect("purple")}>Purple</Button>
+                                        <Button value="Baby Pink" onClick={() => onThemeSelect("pink")}>Pink</Button>
+                                    </div>
+                                }
+                            </div>
                         </div>
 
-                    </div>
-                    <Menu className='menu-item'>
-                        <MenuItem> <p onMouseEnter={() => handleMouseEnter("Mode")}> <span className='menu-icons'><GiArtificialIntelligence /></span>Mode</p></MenuItem>
-                        <MenuItem> <p onMouseEnter={() => handleMouseEnter("Theme")}> <span className='menu-icons'><IoColorPalette /></span>Theme</p></MenuItem>
-                    </Menu>
-                </Sidebar>
+                    </Offcanvas.Body>
 
-                <div className="controls">
-                    {menu.modeMenu &&
-                        <div className="menu" onMouseLeave={handleMouseLeave}>
-                            <Button value="Two Players" onClick={() => setMode("analyze")}>
-                                <span className='menu-icons'><FaUserFriends /></span> Two Players</Button>
-                            <Button value="Computer" onClick={() => setMode("random")}><span className='menu-icons'><FaRobot /></span> Computer</Button>
-                        </div>
-                    }
 
-                    {menu.themeMenu &&
-                        <div className="menu" onMouseLeave={handleMouseLeave}>
-                            <Button value="Red" onClick={() => onThemeSelect("#769656")}>Default</Button>
-                            <Button value="Red" onClick={() => onThemeSelect("red")}>Red</Button>
-                            <Button value="Purple" onClick={() => onThemeSelect("purple")}>Purple</Button>
-                            <Button value="Baby Pink" onClick={() => onThemeSelect("pink")}>Pink</Button>
-                        </div>
-                    }
-                </div>
+                </Offcanvas>
+
+
 
 
             </div >
