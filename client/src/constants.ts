66260610b7b40142399f2fd3107fa17d8fadd77a
Satyam Gupta <satyamgupta1495@gmail.com>
2024-05-26 01:02:47 +0530

    Change URL

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,3 +1,2 @@
-// export const URL = "https://chess-mate-production.up.railway.app"
-export const URL = "http://localhost:3000"
+export const URL = window.location.hostname === '127.0.0.1' ? "http://localhost:3000" : "https://chess-mate-production.up.railway.app";
 
